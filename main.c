1b41ac6c4a67918273030b5fe5dffeac38937bfd
Zhangqun Ming <north_sea@qq.com>
2022-01-20 11:26:56 +0000

    Jump into ram from flash.

--- main.c
+++ main.c
@@ -7,10 +7,29 @@
 #include "hardware/structs/ioqspi.h"
 
 
+#if (RP2040_FACTORY)
+extern uint8_t bin[];
+extern uint32_t bin_size;
+#endif
+
 int main() {
     uint32_t count = 0;
 
     stdio_init_all();
+
+#if (RP2040_FACTORY)
+    {
+        uint32_t i = 0;
+        uint8_t *pu8 = (uint8_t*)0x20000000;
+
+        for (i=0; i<bin_size; i++) {
+            pu8[i] = bin[i];
+        }
+
+        printf("Jump to ram: 0x%x\n", (uint32_t)pu8);
+        watchdog_reboot((uint32_t)pu8, SRAM_END, 0);
+    }
+#endif
 
     while (true) {
 #if (RP2040_FACTORY)
